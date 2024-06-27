3460ef9a4c8ba49576a7e1dd58251a9bba8a8f0a
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-06-27 20:52:27 +1000

    Implement simple jumping

---
+++ js/app.js
@@ -0,0 +1,45 @@
+function main() {
+    const dino = document.querySelector('.dino');
+    const grid = document.querySelector('.grid');
+    let position = 0;
+    let gravity = 0.9;
+    let isJumping = false;
+
+    function control(e) {
+        if (e.code === "Space") {
+            jump();
+        }
+    }
+    
+    function jump() {
+        let count = 0;
+
+        isJumping = true;
+
+        let timerId = setInterval(function() {
+            // Move down
+            if (count === 15) {
+                clearInterval(timerId);
+                let downTimerId = setInterval(function() {
+                    if (count === 0) {
+                        clearInterval(downTimerId);
+                        isJumping = false;
+                    }
+                    position -= 5;
+                    count--;
+                    position *= gravity;
+                    dino.style.bottom = position + 'px';
+                });
+            }
+            //move up
+            position += 16;
+            position *= gravity;    // slow down over time
+            count++;
+            dino.style.bottom = position + 'px';
+        }, 20); // 20ms
+    }
+
+    document.addEventListener('keyup', control);
+}
+
+document.addEventListener('DOMContentLoaded', main);
